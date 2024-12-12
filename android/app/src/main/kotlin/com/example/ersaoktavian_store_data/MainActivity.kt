c073cb5874bdb7a2a7a3563cb3f2efc20bb999cd
ersaoktavian <ersaoktavianramadan@gmail.com>
2024-12-13 05:55:25 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/ersaoktavian_store_data/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ersaoktavian_store_data
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import './pizza.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter JSON Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false, // Menghilangkan debug banner
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  List<Pizza> myPizzas = []; // List untuk menyimpan objek Pizza
+
+  @override
+  void initState() {
+    super.initState();
+    readJsonFile().then((value) {
+      setState(() {
+        myPizzas = value;
+      });
+    });
+  }
+
+  Future<List<Pizza>> readJsonFile() async {
+    String myString = await DefaultAssetBundle.of(context)
+        .loadString('assets/pizzalist.json');
+    List pizzaMapList = jsonDecode(myString); // Decode JSON menjadi Map
+    List<Pizza> pizzas = [];
+    for (var pizza in pizzaMapList) {
+      pizzas.add(Pizza.fromJson(pizza)); // Konversi Map menjadi objek Pizza
+    }
+    return pizzas;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'JSON',
+          style: TextStyle(
+            color: Colors.white, // Warna teks
+          ),
+        ),
+        backgroundColor: Colors.blue, // Warna seluruh latar belakang AppBar
+      ),
+      body: ListView.builder(
+        itemCount: myPizzas.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(myPizzas[index].pizzaName),
+            subtitle: Text(
+              '${myPizzas[index].description} - €${myPizzas[index].price.toStringAsFixed(2)}',
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
